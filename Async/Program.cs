1b3eeabf7feef1e0ec64edd2787f8db10be91753
maris333 <animaral@gmail.com>
2023-05-28 20:29:38 +0200

    Initial commit

--- Async/Program.cs
+++ Async/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿public class Async
+{
+
+    public string DownloadAndProcess(string url)
+    {
+        var client = new HttpClient();
+        var response = client.GetAsync(url).Result;
+        var content = response.Content.ReadAsStringAsync().Result;
+        return content.ToUpper();
+    }
+}
